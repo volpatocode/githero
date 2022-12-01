987d9a14422bb834a6593f125027dea8eaaec150
João Volpato <volpatocode@gmail.com>
2022-12-01 18:42:09 -0300

    feat: logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function index() {
+  return (
+    <Link
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: ".2rem",
+        color: "rgba(57, 211, 83, .9)",
+        alignItems: "center",
+        background: "none",
+        cursor: "pointer",
+        fontSize: "1.5rem",
+      }}
+      href="/"
+    >
+      <Image width={25} height={30} alt="logo" src="/crown.svg" />
+      githero
+    </Link>
+  );
+}
