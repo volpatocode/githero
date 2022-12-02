31cd16882c5ade6979d01012276f004172005404
João Volpato <volpatocode@gmail.com>
2022-12-02 04:21:07 -0300

    fix: hydrations mismatch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
