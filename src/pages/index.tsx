39b8607f217ef42a1e255d079618d20f76a21e58
João Volpato <volpatocode@gmail.com>
2022-12-06 22:10:01 -0300

    feat: home page first look

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,56 @@
 import Head from "next/head";
 import GitGraph from "../components/GitGraph";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import {
+  PageWrapper,
+  Content,
+  MainTitle,
+  MainSubtitle,
+  InfoWrapper,
+  TitleEmphasis,
+  ButtonsBox,
+  Button,
+  SectionTitle,
+  EventsWrapper,
+  Section,
+} from "../styles/homestyles";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home</title>
+        <link rel="icon" type="image/x-icon" href="/crown.svg"></link>
       </Head>
-      <Navbar/>
-      <GitGraph/>
+      <PageWrapper>
+        <Navbar />
+        <Content>
+          <InfoWrapper>
+            <MainTitle>
+              <TitleEmphasis>Gamify</TitleEmphasis> the way you study.
+              <TitleEmphasis> Study</TitleEmphasis> the way you gamify.
+            </MainTitle>
+            <MainSubtitle>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+              nostrum harum quo quaerat mollitia. Optio ullam necessitatibus
+              porro repellat cumque, dolorem ad sint.
+            </MainSubtitle>
+            <ButtonsBox>
+              <Button variant="text">About</Button>
+              <Button variant="contained">Start now!</Button>
+            </ButtonsBox>
+          </InfoWrapper>
+          <Section>
+            <SectionTitle>Active Events</SectionTitle>
+            <EventsWrapper>
+
+            </EventsWrapper>
+          </Section>
+        </Content>
+        <Footer />
+      </PageWrapper>
     </>
   );
 }
