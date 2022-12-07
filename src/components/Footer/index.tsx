94cfa302f91d5d20273b88b96ce826a6293ab19c
João Volpato <volpatocode@gmail.com>
2022-12-06 22:09:10 -0300

    feat: footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import { Footer, Copyright } from "./styles";
+
+import Logo from "../Logo";
+
+export default function index() {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <Footer>
+      <Logo />
+      <Copyright>Copyright &copy; githero{year}. All Right Reserves</Copyright>
+    </Footer>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: #0d0d0d;
+  width: 100%;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #21262d;
+`;
+
+export const Copyright = styled.p`
+  display: grid;
+  padding: 1rem;
+  place-items: center;
+  color: rgba(255, 255, 255, 0.6);
+`;
