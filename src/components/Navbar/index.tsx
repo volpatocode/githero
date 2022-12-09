ea67a12b7aa7cfed30bfc56292690c6214774c00
João Volpato <volpatocode@gmail.com>
2022-12-09 20:58:03 -0300

    feat: add dropdown menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,16 @@
-import { Navbar, Stack, Anchor, Button } from "./styles";
+import { Navbar, Stack, Button } from "./styles";
 
 import Logo from "../Logo";
+import Menu from "../Menu";
 
 export default function index() {
   return (
     <Navbar>
       <Logo />
       <Stack>
-        <Anchor>Home</Anchor>
-        <Anchor>Events</Anchor>
-        <Anchor>Profile</Anchor>
-      </Stack>
-      <Stack>
         <Button variant="text">Sign in</Button>
         <Button variant="contained">Sign Up</Button>
+        <Menu />
       </Stack>
     </Navbar>
   );
