b9607a1c107dbe522e4f15d3e3e2bfbec97d5a7e
João Volpato <volpatocode@gmail.com>
2022-12-12 19:20:15 -0300

    changes: add default to avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,15 +1,27 @@
+import Icon from "@mdi/react";
 import Link from "next/link";
+import { mdiAccountCircle } from "@mdi/js";
 import { Avatar } from "./styles";
 
 type avatarType = {
-    img:string;
-    href: string;
-}
+  img?: string;
+  href?: string;
+  size?: number
+};
 
-export default function index({img, href}:avatarType) {
+export default function index({ img, href, size }: avatarType) {
   return (
-    <Link href={href}>
-      <Avatar src={img}/>
+    <Link href={"/" || href}>
+      {img ? (
+        <Avatar src={img} />
+      ) : (
+        <Icon
+          path={mdiAccountCircle}
+          title="account-circle"
+          size={size || 1.5}
+          color="rgba(255, 255, 255, 0.95)"
+        />
+      )}
     </Link>
   );
 }
