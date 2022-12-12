f82eb8af1b7d59001ad39b3a5033f3b094bd648a
João Volpato <volpatocode@gmail.com>
2022-12-12 19:17:25 -0300

    changes: head, meta, and link tags according to nextjs documentation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { UserContextProvider } from "../contexts/UserContext";
+import { UtilsContextProvider } from "../contexts/UtilsContext";
 import GlobalStyle from "../styles/globalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>GitHero</title>
+        <link rel="icon" type="image/x-icon" href="/crown.svg" />
+      </Head>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <UtilsContextProvider>
+        <UserContextProvider>
+          <Component {...pageProps} />
+        </UserContextProvider>
+      </UtilsContextProvider>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import GitGraph from "../components/GitGraph";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -24,52 +22,42 @@ import Icon from "@mdi/react";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home</title>
-        <link rel="icon" type="image/x-icon" href="/crown.svg"></link>
-      </Head>
-      <PageWrapper>
-        <Navbar />
-        <Content>
-          <InfoWrapper>
-            <MainTitle>
-              <TitleEmphasis>Gamify</TitleEmphasis> the way you study.
-              <TitleEmphasis> Study</TitleEmphasis> the way you gamify.
-            </MainTitle>
-            <MainSubtitle>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-              nostrum harum quo quaerat mollitia. Optio ullam necessitatibus
-              porro repellat cumque, dolorem ad sint.
-            </MainSubtitle>
-            <ButtonsBox>
-              <Button variant="text">About</Button>
-              <Button variant="contained">Create Event!</Button>
-            </ButtonsBox>
-          </InfoWrapper>
-          <Section>
-            <SectionTitle>
-              Active Events
-              <Anchor href="/events">
-                Show more
-                <Icon
-                  path={mdiChevronRight}
-                  title="Arrow Right"
-                  size={1}
-                />
-              </Anchor>
-            </SectionTitle>
-            <EventsWrapper>
-              <CardEvent />
-              <CardEvent />
-              <CardEvent />
-              <CardEvent />
-              <CardEvent />
-            </EventsWrapper>
-          </Section>
-        </Content>
-        <Footer />
-      </PageWrapper>
-    </>
+    <PageWrapper>
+      <Navbar />
+      <Content>
+        <InfoWrapper>
+          <MainTitle>
+            <TitleEmphasis>Gamify</TitleEmphasis> the way you study.
+            <TitleEmphasis> Study</TitleEmphasis> the way you gamify.
+          </MainTitle>
+          <MainSubtitle>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            nostrum harum quo quaerat mollitia. Optio ullam necessitatibus porro
+            repellat cumque, dolorem ad sint.
+          </MainSubtitle>
+          <ButtonsBox>
+            <Button variant="text">About</Button>
+            <Button variant="contained">Create Event!</Button>
+          </ButtonsBox>
+        </InfoWrapper>
+        <Section>
+          <SectionTitle>
+            Active Events
+            <Anchor href="/events">
+              Show more
+              <Icon path={mdiChevronRight} title="Arrow Right" size={1} />
+            </Anchor>
+          </SectionTitle>
+          <EventsWrapper>
+            <CardEvent />
+            <CardEvent />
+            <CardEvent />
+            <CardEvent />
+            <CardEvent />
+          </EventsWrapper>
+        </Section>
+      </Content>
+      <Footer />
+    </PageWrapper>
   );
 }
