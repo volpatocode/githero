183a5bf28f41bc8ef13ea1742c4391ccd66f9f56
João Volpato <volpatocode@gmail.com>
2022-12-12 19:21:52 -0300

    feat: create account page

---
+++ src/pages/createaccount.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Content, PageWrapper } from "../styles/createaccountstyles";
+
+export default function createaccount() {
+  return (
+    <PageWrapper>
+      <Navbar />
+      <Content>
+
+      </Content>
+      <Footer/>
+    </PageWrapper>
+  );
+}
