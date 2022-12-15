6ebaf8fb49914ddd5d7bea6dd2333c02629f6135
João Volpato <volpatocode@gmail.com>
2022-12-15 20:13:02 -0300

    feat: button component types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,35 @@
 export type UserFormData = {
-  username?: string;
+  displayName?: string;
   email?: string;
   password?: string;
   confirmPassword?: string;
   photoURL?: string;
 };
 
-export type buttonType = {
+export type anchorType = {
   variant: "text" | "contained";
+  href: string;
 };
 
+export type buttonType = {
+  text?: string;
+  variant?: "google" | "github";
+  bgColor?: "transparent" | "green" | "blue" | "white" | "black";
+  color?: "transparent" | "green" | "blue" | "white" | "black";
+  border?: "transparent" | "green" | "blue" | "white" | "black";
+  borderRadius?: "sm" | "md" | "lg" | "xl" | "full";
+  fontSize?: "sm" | "md" | "lg" | "xl" | "full";
+  padding?: "sm" | "md" | "lg";
+  image?: any;
+  width?: string;
+  bold?: boolean;
+};
+
+export type menuType = {
+  handleMenu?: () => void;
+  setIsMenuOpen?: (oldState: boolean) => void;
+};
+
+export type navbarType = {
+  anchors?: boolean | true;
+};
